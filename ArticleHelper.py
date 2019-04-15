729af317d932a57480ef1ae59cb48f45c6e235c1
Phillip Jefferies <Phillip.1997@live.com>
2019-04-15 12:01:04 +0100

    Add files via upload

---
+++ ArticleHelper.py
@@ -0,0 +1,59 @@
+import pandas
+import numpy
+import Generic_parser
+
+def formatData(StockData, gcloudcon):
+
+    StockData = StockData.values
+    x = [0] * 31
+    y = StockData[0, 1]
+
+    for n in range(1, len(StockData) - 1):
+        NewsArticles = pandas.read_sql_query(
+            "SELECT * FROM articles WHERE date >= '{0}' AND date < '{1}'".format(StockData[n, 0], StockData[n + 1, 0]),
+            con=gcloudcon)
+        NewsArticles = NewsArticles.values
+
+        date = n
+
+        LMheadline = []
+        LMarticle = []
+        isArticles = False
+        # Sentiment of the news articles
+        for i in range(len(NewsArticles)):
+            isArticles = True
+            LMheadline.append(Generic_parser.get_data(NewsArticles[i, 1].upper()))
+
+            # Check there is an article body, is not just 0 it
+            if NewsArticles[i, 2] is not "":
+                LMarticle.append(Generic_parser.get_data(NewsArticles[i, 2].upper()))
+            else:
+                LMarticle.append([0] * 17)
+
+        if isArticles is True:
+            LMheadline = averageArray(LMheadline)
+            LMarticle = averageArray(LMarticle)
+            LM = LMheadline[2:len(LMheadline)] + LMarticle[2:len(LMarticle)]
+            date = [date] + LM
+            x = numpy.vstack((x, date))
+        else:
+            date = [date] + ([0] * 30)
+            x = numpy.vstack((x, date))
+
+        y = numpy.vstack((y, StockData[n, 1]))
+    return x, y
+
+# Found on github
+def averageArray(array):
+    ncols = len(array[0])
+    nrows = len(array)
+    # Sum all elements in each column:
+    results = ncols * [0]  # sums per column, afterwards avgs
+    for col in range(ncols):
+        for row in range(nrows):
+            results[col] += array[row][col]
+    # Then calculate averages:
+    # * nrows is also number of elements in every col:
+    nelem = float(nrows)
+    results = [s / nelem for s in results]
+    return results
--- Main.py
+++ Main.py
@@ -3,27 +3,78 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+from sklearn.svm import SVR
 from numpy import array
+from datetime import datetime
+from sklearn.model_selection import TimeSeriesSplit
+from sklearn import model_selection
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 import pymysql
 import pandas
-import Generic_parser
+import ArticleHelper
 import numpy
 
-class Article(object):
+def SVR_gen(date, price):
 
-    def __init__(self, df):
-        data = [0] * 5
-        x = 0
-        for item in df[1].iteritems():
-            data[x] = item[1]
-            x += 1
-        
-        self.date = data[0]
-        self.headline = data[1]
-        self.article = data[2]
-        self.link = data[3]
-        self.sentiment = data[4]
+    rbfSVR = SVR(kernel='rbf', C=1e3, gamma=0.1)
 
+    # Turn the price array into a 1D array so sklearn doesnt shout at me
+    price = price.flatten()
+
+    tscv = TimeSeriesSplit(n_splits=10)
+
+    for train_index, test_index in tscv.split(date):
+        X_train, X_test = date[train_index], date[test_index]
+        y_train, y_test = price[train_index], price[test_index]
+
+        rbfSVR.fit(X_train, y_train)
+        plt.scatter(X_train[:,0], y_train, color='black', label='train data')
+        plt.scatter(X_test[:,0], y_test, color='blue', label='test data')
+        plt.plot(X_test[:,0], y_test, color='red', label='real values')
+        plt.plot(X_train[:,0], rbfSVR.predict(X_train), color='yellow', label='rbf')
+        plt.plot(X_test[:,0], rbfSVR.predict(X_test), color='green', label='rbf prediction')
+
+        plt.xlabel('Date')
+        plt.ylabel('Price')
+
+        plt.legend()
+    #     #plt.show()
+
+    #rbfSVR.fit(date, price)
+    kfold = model_selection.KFold(n_splits=10)
+    results = model_selection.cross_val_score(rbfSVR, date, price, cv=kfold, scoring='neg_mean_absolute_error')
+    print("SVR MAE: {0} ({1})".format(results.mean(), results.std()))
+    results = model_selection.cross_val_score(rbfSVR, date, price, cv=kfold, scoring='neg_mean_squared_error')
+    print("SVR MSE: {0} ({1})".format(results.mean(), results.std()))
+    results = model_selection.cross_val_score(rbfSVR, date, price, cv=kfold, scoring='r2')
+    print("SVR R^2: {0} ({1})".format(results.mean(), results.std()))
+
+    return
+
+
+def ML(date, price):
+    model = Sequential()
+    model.add(LSTM(units=len(date[0]),  input_shape=(len(date), len(date[0]))))
+
+    model.compile(loss='mse',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+
+    date = date.reshape(1, len(date), len(date[0]))
+    price = price.flatten()
+    price = price.reshape(1, len(price))
+
+    print(model.input_shape)
+    print(date.shape)
+    print(model.output_shape)
+    print(price.shape)
+
+    model.fit(date, price)
+
+    loss_and_metrics = model.evaluate(date, price, batch_size=128)
+    print(loss_and_metrics)
 
 gcloudcon = pymysql.connect(host='127.0.0.1',
                             database='store',
@@ -31,47 +82,24 @@ gcloudcon = pymysql.connect(host='127.0.0.1',
                             password='3dWHUFePz9dHkFn')
 cur = gcloudcon.cursor()
 
-NewsArticles = pandas.read_sql_query('SELECT * FROM articles WHERE article IS NOT NULL', con=gcloudcon)
-StockData = pandas.read_sql_query('SELECT * FROM dji', con=gcloudcon,index_col='date')
-#abc = NewsArticles.iloc[::-1]
-Xclose = numpy.array(object)
-X = numpy.array(object)
-Y = numpy.array(object)
+#NewsArticles = pandas.read_sql_query('SELECT * FROM articles WHERE article IS NOT NULL', con=gcloudcon)
+StockData = pandas.read_sql_query('SELECT * FROM dji', con=gcloudcon)
 
-StockData = StockData.values
-X = StockData[0, 3]
-Y = StockData[1, 3]
-for n in range(1, len(StockData) - 1):
-    X = numpy.vstack((X, StockData[n, 3]))
-    Y = numpy.vstack((Y, StockData[n + 1, 3]))
+date = numpy.array(object)
+price = numpy.array(object)
 
-        
-scaler = MinMaxScaler(feature_range=(-1, 1))
+date, price = ArticleHelper.formatData(StockData, gcloudcon)
 
-X = scaler.fit_transform(X)
-Y = scaler.fit_transform(Y)
-model = Sequential()
-#Y = Y[:, 0]
-#print(X.shape)
 
-lengthX = len(X)
-samples = list()
-samples.append(X)
-X = array(samples)
-X = X.reshape((-1, lengthX, 1))
-
-lengthY = len(Y)
-Y = Y.reshape((1, lengthY))
-
-print(X.shape)
-print(Y.shape)
-
-model.add(LSTM(4, input_shape=(lengthX, 1)))
-model.add(Dense(units=20, activation='relu'))
-model.add(Dense(units=1, activation='relu'))
-
-model.compile(loss="binary_crossentropy", optimizer='adam', metrics=['accuracy'])
-print(model.input_shape)
-print(model.output_shape)
-model.fit(X, Y, batch_size=20, epochs=1000)
+#Just dates and price
+hold = date[:,0]
+hold = hold.reshape(-1, 1)
+print("SVR based off close prices")
+#value = SVR_gen(hold, price)
+print("SVR close prices and news articles")
+#value = SVR_gen(date, price)
+print("Neural network based off close prices")
+#ML(hold, price)
+print("Neural network based off close prices and news articles")
+ML(date, price)
 
